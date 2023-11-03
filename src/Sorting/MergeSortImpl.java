8fccb320ccbfe8ca78fb7d7bf96820556cf832f9
Faycal Kilali <53886202+faycalki@users.noreply.github.com>
2023-11-03 15:13:24 -0300

    MergeSort addition

---
+++ src/Sorting/MergeSortImpl.java
@@ -0,0 +1,28 @@
+package Sorting;
+
+public class MergeSortImpl {
+
+    /**
+     * Implementation of the specification of merge from the MergeSort Interface.
+     * @implNote Merges two sorted subarrays into a single sorted subarray.
+     * @param array      the array containing the subarrays to be merged
+     * @param leftFirst  the index of the first element in the left subarray
+     * @param leftLast   the index of the last element in the left subarray
+     * @param rightFirst the index of the first element in the right subarray
+     * @param rightLast  the index of the last element in the right subarray
+     */
+    public static void merge(int[] array, int leftFirst, int leftLast, int rightFirst, int rightLast) {
+        
+    }
+
+
+    /**
+     * Sorts the specified range of the array using the Merge Sort algorithm.
+     * @param array the array to be sorted
+     * @param first the index of the first element (inclusive) in the range to be sorted
+     * @param last  the index of the last element (inclusive) in the range to be sorted
+     */
+    public void mergeSort(int[] array, int first, int last) {
+
+    }
+}
